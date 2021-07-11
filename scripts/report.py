dcbd21a0d7648b1c9738336d7a304b4e277708e0
tianwen2 <tianwen1@pm.me>
2021-07-11 14:36:34 -0400

    show version, fix some versions

--- scripts/report.py
+++ scripts/report.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
-import argparse
-import json
-import sys
-
 """
 Report basic statistics on FSD videos.
 
 Usage: ./scripts/report.py videos/*.json
 """
+
+import argparse
+import json
+import re
+import sys
+
+
+# Return (major, minor, version_string?)
+version_re = re.compile("(\\d+)[.](\\d+)(.+)?")
+def get_version(version):
+    m = version_re.match(version)
+    major = int(m.group(1))
+    minor = int(m.group(2))
+    version_string = None if not m.group(3) else m.group(3).strip().replace('(', '').replace(')', '')
+    return (major, minor, version_string)
+
 
 if __name__ == '__main__':
     event_files = sys.argv[1:]
@@ -17,5 +29,7 @@ if __name__ == '__main__':
         with open(event_file) as fin:
             try:
                 d = json.load(fin)
+                version = get_version(d["metadata"]["fsd-version"])
+                print(f"{event_file}: {version}")
             except Exception as e:
                 print(f"Failed to load: {event_file}: {e}")
