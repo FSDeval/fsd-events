724e9c05ed45f7c2cbc106326f6fec4d68bfe801
tianwen2 <tianwen1@pm.me>
2021-07-11 14:27:31 -0400

    Read event files; fix 2 event files

--- scripts/report.py
+++ scripts/report.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
+import json
+import sys
+
 """
 Report basic statistics on FSD videos.
+
+Usage: ./scripts/report.py videos/*.json
 """
 
 if __name__ == '__main__':
-    print("Hello")
+    event_files = sys.argv[1:]
+    print(f"From {len(event_files)} Tesla FSD videos")
+    for event_file in event_files:
+        with open(event_file) as fin:
+            try:
+                d = json.load(fin)
+            except Exception as e:
+                print(f"Failed to load: {event_file}: {e}")
